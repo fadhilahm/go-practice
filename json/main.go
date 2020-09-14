28dcd6914139e098d5355ef5c5648c40710e6275
fadhilahm <fadhilahmetra@gmail.com>
2020-09-15 00:00:11 +0700

    Monday night checkpoint

--- json/main.go
+++ json/main.go
@@ -4,7 +4,9 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"encoding/json"
+	"bytes"
 )
 
 // Student declares `Student` structure
@@ -289,3 +291,338 @@ import (
 // }
 
 // --- handling complex data
+
+// Profile declares `Profile` structure
+// type Profile struct {
+// 	Username		string
+// 	followers		string
+// }
+
+// // Student declares `Student` structure
+// type Student struct {
+// 	FirstName, lastName		string
+// 	HeightInMeters			float64
+// 	IsMale					bool
+// 	Languages				[2]string
+// 	Subjects				[]string
+// 	Grades					map[string]string
+// 	Profile					Profile	
+// }
+
+// func main() {
+
+// 	// some JSON data
+// 	data := []byte(`
+// 	{
+// 		"FirstName": "John",
+// 		"HeightInMeters": 1.75,
+// 		"IsMale": null,
+// 		"Languages": [ "English", "Spanish", "German" ],
+// 		"Subjects": [ "Math", "Science" ],
+// 		"Grades": { "Math": "A" },
+// 		"Profile": {
+// 			"Username": "johndoe91",
+// 			"Followers": 1975
+// 		}
+// 	}`)
+
+// 	// create data container
+// 	var john Student = Student{
+// 		IsMale: true,
+// 		Subjects: []string{ "Art" },
+// 		Grades: map[string]string{ "Science": "A+"},
+// 	}
+
+// 	// unmarshal `data`
+// 	fmt.Printf("Error: %v\n", json.Unmarshal(data, &john))
+
+// 	// print `john` struct
+// 	fmt.Printf("%#v\n", john)
+// }
+
+// -- pointer
+
+// Profile declares `Profile` structure
+// type Profile struct {
+// 	Username	string
+// 	Followers	int
+// }
+
+// // Student declares `Student` structure
+// type Student struct {
+// 	FirstName, lastName		string
+// 	HeightInMeters			float64
+// 	IsMale					bool
+// 	Languages				[2]string
+// 	Subjects				[]string
+// 	Grades					map[string]string
+// 	Profile					*Profile
+// }
+
+// func main() {
+// 	// some JSON data
+// 	data := []byte(`
+// 	{
+// 		"FirstName": "John",
+// 		"HeightInMeters": 1.75,
+// 		"IsMale": null,
+// 		"Languages": [ "English" ],
+// 		"Subjects": [ "Math" , "Science"],
+// 		"Grades": null,
+// 		"Profile": { "Followers": 1975 }
+// 	}`)
+
+// 	// create data container
+// 	var john Student = Student{
+// 		IsMale: true,
+// 		Languages: [2]string{ "Korean", "Chinese" },
+// 		Subjects: nil,
+// 		Grades: map[string]string{ "Math": "A" },
+// 		Profile: &Profile{ Username: "johndoe91" },
+// 	}
+
+// 	// unmarshall `data`
+// 	fmt.Printf("Error: %v\n\n", json.Unmarshal( data, &john ))
+	
+// 	// print `john` struct
+// 	fmt.Printf( "%#v\n\n", john)
+// 	fmt.Printf( "%#v\n", john.Profile)
+// }
+
+// --- promoted fields
+
+// Profile declares `Profile` structure
+// type Profile struct {
+// 	Username 	string
+// 	Followers	int
+// }
+
+// // Account declares `Account` structure
+// type Account struct {
+// 	IsMale bool
+// 	Email string
+// }
+
+// // Student declares `Student` structure
+// type Student struct {
+// 	FirstName, lastName string
+// 	HeightInMeters		float64
+// 	IsMale				bool
+// 	Profile
+// 	Account
+// }
+
+// func main() {
+// 	// some JSON data
+// 	data := []byte(`
+// 	{
+// 		"FirstName": "John",
+// 		"HeightInMeters": 1.75,
+// 		"IsMale": true,
+// 		"Username": "johndoe91",
+// 		"Followers": 1975,
+// 		"Account": { "IsMale": true, "Email": "john@doe.com"}
+// 	}`)
+
+// 	// create a data container
+// 	var john Student
+
+// 	// unmarshall `data`
+// 	fmt.Printf( "Error: %v\n", json.Unmarshal( data, &john ) )
+
+// 	// print `john` struct
+// 	fmt.Printf( "%#v\n", john )
+// }
+
+// --- using structure tags
+
+// Profile declares	`Profile` structure
+// type Profile struct {
+// 	Username	string	`json:"uname"`
+// 	Followers	int		`json:"f_count"`
+// }
+
+// // Student declares `Student` structure
+// type Student struct {
+// 	Username		string		`json:"fname"`
+// 	LastName		string		`json:"-"` // discard
+// 	HeightInMeters	float64		`json:"height"`
+// 	IsMale			bool		`json:"male"`
+// 	Languages		[]string	`json:",omitempy"`
+// 	Profile			Profile		`json:"profile"`
+// }
+
+// func main() {
+// 	// some JSON data
+// 	data := []byte(`
+// 	{
+// 		"fname": "John",
+// 		"LastName": "Doe",
+// 		"height": 1.75,
+// 		"IsMale": true,
+// 		"Languages": null,
+// 		"profile": {
+// 			"uname": "johndoe91",
+// 			"Followers": 1975
+// 		}
+// 	}`)
+
+// 	// create a data container
+// 	var john Student = Student{
+// 		Languages: []string{ "English", "French" },
+// 	}
+
+// 	// unmarshall `data`
+// 	fmt.Printf("Error: %v\n", json.Unmarshal( data, &john ))
+
+// 	// print `john` struct
+// 	fmt.Printf( "%#v\n", john)
+// }
+
+// -- working with maps
+
+// Student declares `Student` map
+// type Student map[string]interface{}
+
+// func main() {
+// 	// some JSON data
+// 	data := []byte(`
+// 	{
+// 		"id": 123,
+// 		"fname": "John",
+// 		"height": 1.75,
+// 		"male": true,
+// 		"languages": null,
+// 		"subjects": [ "Math", "Science" ],
+// 		"profile": {
+// 			"uname": "johndoe91",
+// 			"f_count": 1975
+// 		}
+// 	}`)
+
+// 	// create a data container
+// 	var john Student
+
+// 	// unmarshall `data`
+// 	fmt.Printf("Error: %v\nn", json.Unmarshal( data, &john ))
+
+// 	// print `john` map
+// 	fmt.Printf("%#v\n\n", john)
+
+// 	// iterate through keys and values
+// 	i := 1;
+// 	for k, v := range john {
+// 		fmt.Printf("%d: key ['%T']'%v', value ('%T')'%#v'\n", i, k, k, v, v)
+// 		i++;
+// 	}
+// }
+
+// --- even more complex data
+
+// func main() {
+// 	// some JSON data
+// 	data := []byte(`
+// 	{
+// 		"id": 123,
+// 		"fname": "John",
+// 		"height": 1.75,
+// 		"male": true,
+// 		"languages": null,
+// 		"subjects": [ "Math", "Science" ],
+// 		"profile": {
+// 			"uname": "johndoe91",
+// 			"f_count": 1975
+// 		}
+// 	}`)
+
+// 	// create a data container
+// 	var john interface{}
+// 	fmt.Printf( "Before: `type` of `john` is %T and its `value` is %v\n", john, john)
+
+// 	// unmarshall `data`
+// 	fmt.Printf( "Error: %v\n", json.Unmarshal(data, &john ))
+// 	fmt.Printf( "After: `type` of `john` is %T\n\n", john )
+
+// 	// print `john` map
+// 	fmt.Printf( "%#v\n", john )
+
+// 	// extract the concrete value
+// 	johnData := john.(map[string]interface{})
+
+// 	fmt.Println(johnData)
+// }
+
+// --- using Unmarshaller and TextUnmarshaller
+
+// Profile declares `Profile` structure
+// type Profile struct {
+// 	Username	string
+// 	Followers	string
+// }
+
+// // UnmarshalJSON - implement Unmarshaler interface
+// func ( p *Profile ) UnmarshalJSON( data []byte ) error {
+
+// 	// unmarshall JSON
+// 	var container map [string]interface{}
+// 	_ = json.Unmarshal( data, &container )
+// 	fmt.Printf("container: %T / %#v\n\n", container, container)
+
+// 	// extract interface values
+// 	iuserName, _ := container["Username"]
+// 	ifollowers, _ := container["f_count"]
+// 	fmt.Printf( "iusername: %T/%#v\n", iuserName, iuserName )
+// 	fmt.Printf( "ifollowers: %T/%#v\n\n", ifollowers, ifollowers)
+
+// 	// extract concrete values
+// 	userName, _ := iuserName.(string) // get `string` value
+// 	followers, _ := ifollowers.(float64) // get `float64` value
+// 	fmt.Printf("userName: %T/%#v\n", userName, userName)
+// 	fmt.Printf("followers: %T/%#v\n\n", followers, followers)
+
+// 	// assign values
+// 	p.Username = strings.ToUpper(userName)
+// 	p.Followers = fmt.Sprintf("%.2fk", followers / 1000)
+
+// 	return nil
+// }
+
+// // Student declares `Student` structure
+// type Student struct {
+// 	FirstName	string
+// 	Profile		Profile
+// }
+
+// func main() {
+// 	// some JSON data
+// 	data := []byte(`
+// 	{
+// 		"FirstName": "John",
+// 		"Profile": {
+// 			"Username": "johndoe91",
+// 			"f_count": 1975
+// 		}
+// 	}`)
+
+// 	// create a data container
+// 	var john Student
+
+// 	// unmarshall `data`
+// 	fmt.Printf("Error: %#v\n", json.Unmarshal( data, &john ))
+
+// 	// print `john` struct
+// 	fmt.Printf("%#v\n", john)
+// }
+
+// --- encoder and decoder
+
+// Person declares `Person` structure
+type Person struct {
+	Name 	string
+	Age		string
+}
+
+func main() {
+	// create a buffer to hold JSON data
+	buf := new(bytes.Buffer)
+}
